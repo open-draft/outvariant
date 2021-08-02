1b69d1438893d64113a7a645f1ecd57f1fbf5773
Artem Zakharchenko <kettanaito@gmail.com>
2021-08-02 14:32:20 +0200

    Uses the "format-util" for message interpolation

--- src/invariant.ts
+++ src/invariant.ts
@@ -1,19 +1,12 @@
+import format from 'format-util'
+
 const STACK_FRAMES_TO_IGNORE = 2
-
-export function interpolate(message: string, ...positionals: any[]): string {
-  let index = 0
-
-  return message.replace(/%[s|d|o]/g, (match) => {
-    const value = positionals[index++] ?? match
-    return typeof value === 'object' ? JSON.stringify(value) : value
-  })
-}
 
 export class InvariantError extends Error {
   constructor(message: string, ...positionals: any[]) {
     super(message)
     this.name = 'Invariant Violation'
-    this.message = interpolate(message, ...positionals)
+    this.message = format(message, ...positionals)
 
     if (this.stack) {
       const prevStack = this.stack
